637a9a175a0e0a95c089356d3cc2bb7e559daedb
RacquelO <RacquelO@users.noreply.github.com>
2016-05-07 12:15:15 -0400

    s

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ This file is intended to help developers get their SQL Databases setup correctly
 It is important since other developers will be unlikely to have the same database or tables setup already. 
 */
 
-CREATE DATABASE  `StarWarsDeploy`;
-USE `StarWarsDeploy`;
+CREATE DATABASE  `h8a0zh3nrl04v9ws`;
+USE `h8a0zh3nrl04v9ws`;
 
 CREATE TABLE `allcharacters` (
 	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
